24d2d0a7c884879a08c6ed99dc8a306086c16405
Hayden Sykes <haydensykes13@gmail.com>
2021-04-25 13:00:31 -0400

    add linting and prettier rules.  add gray-matter for post handling

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const nextConfig = {
     target:"serverless",
+    webpack: function(config){
+        config.moudle.rules.push({
+            test:/\.md$/,
+            use:"raw-loader",
+        });
+        return config;
+    }
 };
 
 module.exports = nextConfig;
